9f134f12040e27f96fc68a50c49d7d83b037043d
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-09-16 22:16:13 -0300

    footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer: React.FC = () => {
+  return <Container>Interface developed with ReactJS and TypeScript</Container>;
+};
+
+export default Footer;
